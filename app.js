8f0618c5310754da44c69bbb0159d6d7566675e5
Diana <dianaibarra0303@gmail.com>
2026-02-22 17:26:35 -0600

    modificación del html

--- app.js
+++ app.js
@@ -143,8 +143,3 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   })
 })
-//Line 98 updated
-// "Edit Note" to "Editar nota"
-
-//Line 105 updated
-// "Add New Note" to "Añadir nueva nota"
